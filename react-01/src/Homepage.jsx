3aba8e0a01c89a011458c21081e26e061dc365ce
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2024-02-02 04:28:35 +0000

    react start

---
+++ react-01/src/Homepage.jsx
@@ -0,0 +1,7 @@
+function HomePage(){
+    return <div>
+        <h1>I am a Homepage</h1>
+    </div>
+}
+
+export default HomePage
---
+++ react-01/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HomePage from './Homepage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+   <HomePage></HomePage>
+)
